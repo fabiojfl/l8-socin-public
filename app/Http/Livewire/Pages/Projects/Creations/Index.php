d599503faa664cc4e0a71ecf3cd0d9ecb981bed1
Fábio José <fabiojfl@gmail.com>
2023-05-19 08:03:48 -0300

    make livewire projects for list projects created

---
+++ app/Http/Livewire/Pages/Projects/Creations/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Projects\Creations;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.projects.creations.index');
+    }
+}
