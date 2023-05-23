b6caa1d3fd8c60d3c00163559735e55f55efdc8d
Fábio José <fabiojfl@gmail.com>
2023-05-22 22:26:32 -0300

    start layout hexadash-html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,105 @@
 <x-guest-layout>
+
+
+<main class="main-content">
+        <div class="admin" style="background-image:url({{ asset('assets/img/admin-bg-light.png') }});">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-xxl-3 col-xl-4 col-md-6 col-sm-8">
+                        <div class="edit-profile">
+                            <div class="edit-profile__logos">
+                                <img class="dark" src="{{ asset('assets/img/logo-dark.png') }}" alt="">
+                                <img class="light" src="{{ asset('assets/img/logo-white.png') }}" alt="">
+                            </div>
+                            <div class="card border-0">
+                                <div class="card-header">
+                                    <div class="edit-profile__title">
+                                        <h6>Sign in HexaDash</h6>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <form action="" method="POST">
+                                        @csrf
+                                        <div class="edit-profile__body">
+                                            <div class="form-group mb-20">
+                                                <label for="email">Username Or Email Address</label>
+                                                <input type="text" class="form-control" id="email" name="email" value="admin@gmail.com" placeholder="Email address">
+                                                @if($errors->has('email'))
+                                                    <p class="text-danger">{{$errors->first('email')}}</p>
+                                                @endif
+                                            </div>
+                                            <div class="form-group mb-15">
+                                                <label for="password-field">password</label>
+                                                <div class="position-relative">
+                                                    <input id="password-field" type="password" class="form-control" name="password" placeholder="Password" value="admin">
+                                                    <span toggle="#password-field" class="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2"></span>
+                                                </div>
+                                                @if($errors->has('password'))
+                                                    <p class="text-danger">{{$errors->first('password')}}</p>
+                                                @endif
+                                            </div>
+                                            <div class="admin-condition">
+                                                <div class="checkbox-theme-default custom-checkbox ">
+                                                    <input class="checkbox" type="checkbox" id="check-1">
+                                                    <label for="check-1">
+                                                        <span class="checkbox-text">Keep me logged in</span>
+                                                    </label>
+                                                </div>
+                                                <a href="">forget password?</a>
+                                            </div>
+                                            <div class="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
+                                                <button class="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
+                                                    sign in
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="px-20">
+                                    <p class="social-connector social-connector__admin text-center">
+                                        <span>Or</span>
+                                    </p>
+                                    <div class="button-group d-flex align-items-center justify-content-center">
+                                        <ul class="admin-socialBtn">
+                                            <li>
+                                                <button class="btn text-dark google">
+                                                    <img class="svg" src="{{ asset('assets/img/google-Icon.svg') }}" alt="img" />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <button class=" radius-md wh-48 content-center facebook">
+                                                    <i class="uil uil-facebook-f"></i>
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <button class="radius-md wh-48 content-center twitter">
+                                                    <i class="uil uil-twitter"></i>
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <button class="radius-md wh-48 content-center github">
+                                                    <i class="uil uil-github"></i>
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="admin-topbar">
+                                    <p class="mb-0">
+                                        Don't have an account?
+                                        <a href="{{ route('register') }}" class="color-primary">
+                                            Sign up
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,22 +1,42 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
-        <link  href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
+<!doctype html>
+<html lang=""{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ (Session::get('layout')=='rtl' ? 'rtl' : 'ltr') }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="@yield('description')"/>
+    <title>HexaDash | @yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset( 'assets/css/plugin' . Helper::rlt_ext() . '.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style' . Helper::rlt_ext() . '.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/variables' . Helper::rlt_ext() . '.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app' . Helper::rlt_ext() . '.min.css') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon.png') }}">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.0/css/line.css">
+</head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        
             {{ $slot }}
+        <div id="overlayer">
+        <div class="loader-overlay">
+            <div class="dm-spin-dots spin-lg">
+                <span class="spin-dot badge-dot dot-primary"></span>
+                <span class="spin-dot badge-dot dot-primary"></span>
+                <span class="spin-dot badge-dot dot-primary"></span>
+                <span class="spin-dot badge-dot dot-primary"></span>
+            </div>
         </div>
+    </div>
+    <div class="enable-dark-mode dark-trigger">
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="uil uil-moon"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <script src="{{ asset('assets/js/plugins.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.min.js') }}"></script>
     </body>
 </html>
