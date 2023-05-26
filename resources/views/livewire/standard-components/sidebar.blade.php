65cddbb32f61ec8b29aa0af44f84c82fd8e4dd84
Fábio José <fabiojfl@gmail.com>
2023-05-26 20:44:34 -0300

    added menus in sidebar

--- resources/views/livewire/standard-components/sidebar.blade.php
+++ resources/views/livewire/standard-components/sidebar.blade.php
@@ -1,29 +1,63 @@
 <div class="sidebar-wrapper">
-            <aside class="sidebar sidebar-collapse" id="sidebar">
-            <!-- partials._menu -->
-            <div class="sidebar__menu-group">
-                <ul class="sidebar_nav">
-                    <li class="menu-title mt-30">
-                     <span>Applications</span>
-                    </li>
-                    <li class="has-child">
-                        <a href="#" class="">
-                            <span class="nav-icon uil uil-envelope"></span>
-                            <span class="menu-text">Email</span>
-                            <span class="toggle-icon"></span>
-                        </a>
-                        <ul>
-                            <li class="">
-                            <a href="inbox.html">Inbox</a>
-                            </li>
-                            <li class="">
-                            <a href="read-email.html">Read
-                                Email</a>
-                            </li>
-                        </ul>
+    <aside class="sidebar sidebar-collapse" id="sidebar">
+    <!-- partials._menu -->
+    <div class="sidebar__menu-group">
+        <ul class="sidebar_nav">
+            <li class="menu-title mt-30">
+                <span>Aplicações</span>
+            </li>            
+            <li class="has-child">
+                <a href="#" class="">
+                    <span class="nav-icon uil uil-users-alt"></span>
+                    <span class="menu-text">Usuários</span>
+                    <span class="toggle-icon"></span>
+                </a>
+                <ul>
+                    <li class="">
+                        <a href="users-membar.html">Lista de usuários</a>
                     </li>
                 </ul>
-            </div>   
-            <!-- end partials._menu -->
-            </aside>
-        </div>
+            </li>
+            <li class="has-child">
+                <a href="#" class="">
+                    <span class="nav-icon uil uil-images"></span>
+                    <span class="menu-text">Publicações</span>
+                    <span class="toggle-icon"></span>
+                </a>
+                <ul>
+                    <li class="">
+                        <a href="blog.html">Listar publicações</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-child">
+                <a href="#" class="">
+                    <span class="nav-icon uil uil-arrow-growth"></span>
+                    <span class="menu-text">Investidores</span>
+                    <span class="toggle-icon"></span>
+                </a>
+                <ul>
+                    <li class="">
+                        <a href="blog.html">Listar publicações</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-child">
+                <a href="#" class="">
+                    <span class="nav-icon uil uil-tachometer-fast"></span>
+                    <span class="menu-text">Configurações</span>
+                    <span class="toggle-icon"></span>
+                </a>
+                <ul>
+                    <li class="">
+                        <a href="users-membar.html">Lista de usuários</a>
+                    </li>
+                </ul>
+            </li>
+            
+        </ul>
+    </div>   
+    <!-- end partials._menu -->
+    
+    </aside>
+</div>
